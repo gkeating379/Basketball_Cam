5d48033be88ad36ba5e8b9d95bf271c6fe2f44b0
gkeating <gtkeating@yahoo.com>
2024-06-06 10:49:19 -0400

    Fix: fix styling

--- perspective_estimation/homography.py
+++ perspective_estimation/homography.py
@@ -1,17 +1,15 @@
 import numpy as np
 import cv2
 import math
-import time
-import skimage.draw
 
-#global (based on output image of court)
+# global (based on output image of court)
 base_values = {'top-right': (546, 107), 'top-left': (53, 107), 'bot-left': (53, 1039), 'bot-right': (546, 1039),
                'top-paint-left': (220, 107), 'top-paint-right':(380, 107), 'bot-paint-left': (220,1039), 'bot-paint-right':(380,1039)}
+
 
 def frame_to_2means(frame):
     # Convert the image to a 1D array of floats
     frame = np.copy(frame)
-    #frame = cv2.GaussianBlur(frame,(5,5),2,2)
     data = frame.reshape((-1, 3)).astype(np.float32)
 
     # Perform KMeans clustering
@@ -20,42 +18,54 @@ def frame_to_2means(frame):
     attempts = 1
     flags = cv2.KMEANS_PP_CENTERS
 
-    compactness, labels, centers = cv2.kmeans(data, K, None, criteria, attempts, flags)
+    _, labels, centers = cv2.kmeans(data, K, None, criteria, attempts, flags)
 
     # Convert the centers to integer values and fixed B/W
     centers = np.uint8(centers)
-    centers[0], centers[1] = ([255,255,255], [0,0,0]) if np.sum(centers[0]) < np.sum(centers[1]) else ([0,0,0], [255,255,255])
-    
+    if np.sum(centers[0]) < np.sum(centers[1]):
+        centers[0], centers[1] = ([255, 255, 255], [0, 0, 0])
+    else:
+        centers[0], centers[1] = ([0, 0, 0], [255, 255, 255])
+
     # Assign each pixel its center cluster color
     segmented_image = centers[labels]
 
     # Reshape the segmented image to match the original image shape
     segmented_image = segmented_image.reshape(frame.shape)
 
-    #TODO see if this speeds up overall processing
-    segmented_image = cv2.morphologyEx(segmented_image, cv2.MORPH_CLOSE, np.ones((3, 3), np.uint8), iterations=2)
-    #cv2.imwrite('test.png', segmented_image)
+    segmented_image = cv2.morphologyEx(segmented_image,
+                                       cv2.MORPH_CLOSE,
+                                       np.ones((3, 3), np.uint8),
+                                       iterations=2)
 
     return segmented_image
+
 
 def get_polar_intersection(rho1, theta1, rho2, theta2):
     '''Gets the point where two polar lines intersect
     
     Follows https://math.stackexchange.com/questions/4704222/find-intersection-of-two-lines-in-polar-coordinates'''
-    if theta1 - theta2 == 0: #parallel lines do not intersect
+    if theta1 - theta2 == 0: # parallel lines do not intersect
         return None
     x = (rho2 * math.sin(theta1) - rho1 * math.sin(theta2)) / (math.sin(theta1 - theta2))
     y = (rho1 * math.cos(theta2) - rho2 * math.cos(theta1)) / (math.sin(theta1 - theta2))
 
     return x, y
 
+
 def get_dist(p1, p2):
-    '''Gets euclidean distance'''
+    '''Gets euclidean distance between two same dimension points
+
+    p1 => n dimension vector
+    p2 => n dimension vector
+
+    Return euclidean distance between p1 and p2'''
     out = 0
     for x1, x2 in zip(p1, p2):
         out += (x1 - x2)**2
 
     return out**0.5
+
 
 def get_four_corners(l1, l2, offset=0, im_height=0):
     '''Takes two lines forming the top and side of the court
@@ -64,40 +74,42 @@ def get_four_corners(l1, l2, offset=0, im_height=0):
 
     l1 => top line (length)
     l2 => side line (width)
-    offset => distance alon side line by y offset (eg corner is placed at intercept plus offset on the y)'''
+    offset => distance alon side line by y offset
+              (eg corner is placed at intercept plus offset on the y)'''
 
     length_to_width = 1.88
     rho1, theta1 = l1
     rho2, theta2 = l2
 
-    #p1 is the intersection of the two lines
+    # p1 is the intersection of the two lines
     x1, y1 = get_polar_intersection(rho1, theta1, rho2, theta2)
-    right_side = theta2 > 1 #x1 > im_shape[1]/2
+    right_side = theta2 > 1
 
-    #set offset as 65% of the way off screen
+    # set offset as 65% of the way off screen
     if offset == 0:
-        offset = (y1 - im_height) 
-        offset /= -math.sin(theta2) #if right_side else math.sin(theta2)
+        offset = (y1 - im_height)
+        offset /= -math.sin(theta2)  # if right_side else math.sin(theta2)
         offset *= .65
 
-    #p2 is the point along the width specified by offset
+    # p2 is the point along the width specified by offset
     y2 = y1 + offset
     x2 = (rho2 - y2*math.sin(theta2)) / math.cos(theta2)
 
-    #get length of sides of trapezoid
-    minor_l = get_dist((x1,y1), (x2,y2))
+    # get length of sides of trapezoid
+    minor_l = get_dist((x1, y1), (x2, y2))
     triangle_edge = minor_l * math.cos(theta2) if right_side else -minor_l * math.cos(theta2)
     major_l = minor_l*length_to_width - 2*triangle_edge
 
-    #p3 is the point along the length 
-    x3 = x1 + -major_l * math.sin(theta1) if right_side else x1 + major_l * math.sin(theta1) #negative if on the right
+    # p3 is the point along the length
+    x3 = x1 + -major_l * math.sin(theta1) if right_side else x1 + major_l * math.sin(theta1)
     y3 = y1 + major_l * math.cos(theta1) if right_side else y1 + -major_l * math.cos(theta1)
 
-    #p4 is the diagonal with p1
+    # p4 is the diagonal with p1
     x4 = x3 + -minor_l * math.sin(theta2) if right_side else x3 + minor_l * math.sin(theta2)
     y4 = y3 + -minor_l * math.cos(theta2) if right_side else y3 + minor_l * math.cos(theta2)
 
     return (x1, y1), (x2, y2), (x3, y3), (x4, y4), right_side
+
 
 def draw_point(image, p, color):
     '''Draws a point that may be float or int'''
@@ -106,6 +118,7 @@ def draw_point(image, p, color):
         image[int(y)][int(x)] = color
 
     return image
+
 
 def draw_polar_line(image, line, color):
     '''Draws a line in polar coords'''
@@ -122,6 +135,7 @@ def draw_polar_line(image, line, color):
 
     return image
 
+
 def draw_four(image, p1, p2, p3, p4, color):
     '''Draws the quadraleteral connecting four points'''
     p1 = (int(p1[0]), int(p1[1]))
@@ -136,26 +150,27 @@ def draw_four(image, p1, p2, p3, p4, color):
 
     return image
 
+
 def find_best_projection(image, verts, horzs, court, img_canny):
     '''Finds the best vertical and horizontal bound for the court
     Returns: Homography from current view to top down view
-    
+
     image => base input from camera
     verts => list of vertical lines in polar form [rho, theta]
     horzs => list of horizontal lines in polar form
     court => top down image of court
     img_canny => Canny edges of the frame image'''
-    #compare horizontal and vertical line intersections
-    #find the best two lines to represent court boundaries
+    # compare horizontal and vertical line intersections
+    # find the best two lines to represent court boundaries
     best_err = 0
     proj = []
     for vert in verts:
         for horz in horzs:
-            #drawing for debugging
+            # drawing for debugging
             p1, p2, p3, p4, right_side = get_four_corners(horz, vert, im_height=len(image))
             draw_four(image, p1, p2, p3, p4, (0, 0, 255))
 
-            #determine if court end is on the right
+            # determine if court end is on the right
             if right_side:
                 dst = [base_values['top-left'], base_values['top-right'], base_values['bot-left'], base_values['bot-right']]
             else:
@@ -163,16 +178,17 @@ def find_best_projection(image, verts, horzs, court, img_canny):
 
             H = warp_image([p1, p2, p3, p4], dst, image, court)
 
-            #get average line error            
+            # get average line error  
             cur_err = (eval_line(p1, p2, img_canny) + eval_line(p1, p3, img_canny)) / 2
 
-            #update best H and the points that made it
+            # update best H and the points that made it
             if cur_err > best_err:
                 best_err = cur_err
                 proj = H
-                b1, b2, b3, b4 = p1, p2, p3, p4 
-                
+                b1, b2, b3, b4 = p1, p2, p3, p4
+ 
     return proj, b1, b2, b3, b4
+
 
 def draw_k_best_lines(image, add_k, court):
     # Convert the image to grayscale
@@ -186,8 +202,8 @@ def draw_k_best_lines(image, add_k, court):
     lines = cv2.HoughLines(edges, 1, np.pi / 360, 1)
 
     if lines is not None:
-        lines = lines[:add_k] if len(lines) > add_k else lines  # Get the k best lines or all lines if less than k
-    
+        lines = lines[:add_k] if len(lines) > add_k else lines
+
         # Draw the k best lines on the original image
         verts = []
         horzs = []
@@ -199,16 +215,16 @@ def draw_k_best_lines(image, add_k, court):
                 verts.append(line)
                 draw_polar_line(image, line, [255, 0, 0])
             else:
-                horzs.append(line) 
-                draw_polar_line(image, line, [0, 255, 0])    
-
+                horzs.append(line)
+                draw_polar_line(image, line, [0, 255, 0])
 
         if verts == [] or horzs == []:
             return image, None, None, None, None, None, edges
-        
+
         H, p1, p2, p3, p4 = find_best_projection(image, verts, horzs, court, edges)
 
     return image, H, p1, p2, p3, p4, edges
+
 
 def warp_image(src_points, dst_points, img_from, img_to, display=False):
     '''Computes homography and warps image onto another'''
@@ -223,14 +239,15 @@ def warp_image(src_points, dst_points, img_from, img_to, display=False):
     else:
         return H
 
+
 def get_reprojection_error(src, dst):
     '''Gets the reprojection error between two images
     Src is assumed to have empty regions without importance
-    Consider using the Canny line representations'''  
+    Consider using the Canny line representations'''
 
     on_court = np.where(dst == 255, src, dst)
     out_of_bounds = np.where(dst == 0, src, dst)
-    
+
     lowest_row = np.sum(np.sum(on_court, axis=2), axis=1)
     lowest_row = np.where(lowest_row != len(src[0]) * 255 * 3)[0]
     lowest_row = lowest_row[-1] if len(lowest_row) > 0 else 1
@@ -238,27 +255,29 @@ def get_reprojection_error(src, dst):
     total = np.sum(on_court[:lowest_row]) / lowest_row
     total += np.sum(out_of_bounds[:lowest_row] == 0) / lowest_row
 
-    #total area within the court that is "court colored"
+    # total area within the court that is "court colored"
     return total
+
 
 def eval_line(p1, p2, canny):
     '''Gives a line score based on how well it matches the canny image lines
-    
+
     p1 => first point of line
     p2 => second point of line
     canny => canny image to use for eval
-    
+
     Return score.  Higher score is better fit'''
     line = np.zeros_like(canny)
     line = cv2.line(line, (int(p1[0]), int(p1[1])), (int(p2[0]), int(p2[1])), 255, 2)
-    score = np.sum(np.where(line==255, canny, 0))
+    score = np.sum(np.where(line == 255, canny, 0))
     return score
+
 
 def get_best_frame_homography(frame, court):
     '''Returns the best homography from the frame to the court'''
 
     kmeans = frame_to_2means(frame)
     lines, H, p1, p2, p3, p4, canny = draw_k_best_lines(kmeans, 10, court)
-    cv2.imshow('lines', lines)
+    #cv2.imshow('lines', lines)
 
     return H, p1, p2, p3, p4, canny
--- tracking/tracking.py
+++ tracking/tracking.py
@@ -1,62 +1,91 @@
 import supervision as sv
+from ultralytics import YOLO
 import cv2
 import numpy as np
-from . import jersey
 
 
-font = cv2.FONT_HERSHEY_SIMPLEX
+font = cv2.FONT_HERSHEY_COMPLEX
 font_scale = 1
-font_color = (255,255,255)
-thickness = 1
+font_color = (58, 37, 245)
+thickness = 2
 line_type = 2
+
+
 class Track:
     def __init__(self):    
-        self.tracker = sv.ByteTrack()
+        self.tracker = sv.ByteTrack(track_activation_threshold=0, lost_track_buffer=100, minimum_matching_threshold=0.7)
         self.label = sv.LabelAnnotator()
+        self.jersey_model = YOLO('models\\Jersey\\med\\best.pt')
 
     def get_object_tracks(self, frame, results):
         detections = sv.Detections.from_ultralytics(results)
         tracked_detections = self.tracker.update_with_detections(detections)
-        labels = [ f"{tracker_id}" for tracker_id in tracked_detections.tracker_id ]
-        annotated_frame = self.label.annotate(scene=frame.copy(), detections=tracked_detections, labels=labels)
-
-        cv2.imshow('tracked', annotated_frame)
+        # labels = [ f"{tracker_id}" for tracker_id in tracked_detections.tracker_id ]
+        # print(tracked_detections.tracker_id)
+        # print(len(detections), len(labels))
+        # annotated_frame = self.label.annotate(scene=frame.copy(), detections=tracked_detections, labels=labels)
+        # cv2.imshow('tracked', annotated_frame)
 
         return tracked_detections
 
     def proj_track(self, tracked_detections, H, court, frame):
         '''Draws the circle icons for each detected player onto the court
-        
+
         tracked_detections => current tracked detections in the frame
         H => homography for the frame
         court => image to warp to
-        frame => image of frame'''
+        '''
         marked = np.copy(court)
         for i, id in enumerate(tracked_detections.tracker_id):
-            #project the coords (middle of bottom of box)
+            # project the coords (middle of bottom of box)
             y = tracked_detections.xyxy[i][3] - (tracked_detections.xyxy[i][3] - tracked_detections.xyxy[i][3])/10
-            x = (tracked_detections.xyxy[i][2] + tracked_detections.xyxy[i][0])/2 
-            court_coords = np.matmul(H, np.atleast_2d([x, y, 1]).T).T[0] #its ugly I hate np 1d arrays
+            x = (tracked_detections.xyxy[i][2] + tracked_detections.xyxy[i][0])/2
+            court_coords = np.matmul(H, np.atleast_2d([x, y, 1]).T).T[0]  # its ugly I hate np 1d arrays
             court_coords = (court_coords/court_coords[2])[:2]
             court_coords = np.ndarray.astype(court_coords, np.int64)
 
-            #get jersey number
+            # get jersey number (didn't work well)
+            # jersey.get_jersey_number(frame, tracked_detections.xyxy[i], self.jersey_model)
 
-            #draw icon onto court
+            # draw icon onto court
             r = 20
-            cv2.circle(marked, court_coords, r, [252, 98, 3])
+            cv2.circle(marked, court_coords, r, [0, 0, 0], 2)
 
-            #get the bottom-left corner of the text
+            # get the bottom-left corner of the text
             (text_width, text_height), baseline = cv2.getTextSize(f'{id}', font, font_scale, thickness)
             text_x = court_coords[0] - text_width // 2
             text_y = court_coords[1] + text_height // 2
 
-            cv2.putText(marked, f'{id}', 
-                (text_x, text_y), 
-                font, 
-                font_scale,
-                font_color,
-                thickness,
-                line_type)
+            cv2.putText(marked, f'{id}',
+                        (text_x, text_y),
+                        font,
+                        font_scale,
+                        font_color,
+                        thickness,
+                        line_type)
 
-        cv2.imshow('projas', marked)
+        cv2.imshow('Players on Court', marked)
+
+    def player_interp(bound, last_frame, frame):
+        '''Attempt to track a bounding box into the new frame
+        Used to find previous detections that the model cannot find'''
+        (x1, y1, x2, y2) = bound
+
+        # block out non bounding box
+        mask = np.zeros_like(last_frame)
+        mask[y1:y2, x1:x2] = 1
+
+        last_features = cv2.goodFeaturesToTrack(last_frame, maxCorners=20, qualityLevel=0.1, minDistance=3, mask=mask, blockSize=5)
+        new_feats, _, _ = cv2.calcOpticalFlowPyrLK(last_frame, frame, last_features, None, winSize=(11,11), maxLevel=3, criteria=(cv2.TERM_CRITERIA_COUNT + cv2.TERM_CRITERIA_COUNT,50,0.1))
+
+        x_dist = np.average(new_feats[:, 0])
+        y_dist = np.average(new_feats[:, 1])
+
+        x1 += x_dist
+        x2 += x_dist
+        y1 += y_dist
+        y2 += y_dist
+
+        cv2.imshow('Interp', frame[y1:y2, x1:x2])
+
+        return (x1, y1, x2, y2)
