0b91507c3e6ede1093f9d6b1d1f15d63ee0f9bb8
gkeating <gtkeating@yahoo.com>
2024-06-06 10:41:02 -0400

    Fix: refactor motion.py

--- perspective_estimation/motion.py
+++ perspective_estimation/motion.py
@@ -2,107 +2,143 @@ import cv2
 import numpy as np
 from . import homography
 
-base_values = {'top-right': (546, 107), 'top-left': (53, 107), 'bot-left': (53, 1039), 'bot-right': (546, 1039),
-               'top-paint-left': (220, 107), 'top-paint-right':(380, 107), 'bot-paint-left': (220,1039), 'bot-paint-right':(380,1039)}
+base_values = {'top-right': (546, 107), 'top-left': (53, 107),
+               'bot-left': (53, 1039), 'bot-right': (546, 1039),
+               'top-paint-left': (220, 107), 'top-paint-right': (380, 107),
+               'bot-paint-left': (220, 1039), 'bot-paint-right': (380, 1039)}
 
 
 def get_dist(p1, p2):
-    '''Gets euclidean distance'''
+    '''Gets euclidean distance between two same dimension points
+
+    p1 => n dimension vector
+    p2 => n dimension vector
+
+    Return euclidean distance between p1 and p2'''
     out = 0
     for x1, x2 in zip(p1, p2):
         out += (x1 - x2)**2
 
     return out**0.5
 
+
 def update_drift(drift_x, drift_y, points):
-    '''Update the points by adding the drift to each point'''
+    '''Update the points by adding the drift to each point
+
+    drift_x => camera motion in the x axis of the image
+    drift_y => camera motion in the y axis of the image
+    points => points to apply the drift to
+
+    Return the points with the x and y drift applied'''
     for i in range(len(points)):
         points[i][0] += drift_x
         points[i][1] += drift_y
     return points
 
+
 def update_drift_full(M, points):
-    '''Update the points by preforming the affine transform to each point'''
+    '''Update the points by preforming the affine transform to each point
+
+    M => affine matrix
+    points => points to apply the matrix
+
+    Returns M*points'''
     for i in range(len(points)):
         points[i] = np.matmul(M, np.append(points[i], 1).T)
     return np.array(points)
 
+
 def create_mask(im, court_corners, detections):
     '''Creates mask of which regions to include for feature tracking
-    
-    im=> image to mask
+
+    im => image to mask
     court_corners => four corners of court
     detections => list of object detections in xyxy
-    
+
     Return
-    mask => mask over im where 1 is region to not consider'''
+    mask => mask over im where 1 is region to not consider
+            mask out all regions above the court, inside a detection box
+            and too far to the bottom of the image'''
     mask = np.ones_like(im)
 
-    #mask everything above the court
+    # mask everything above the court
     if court_corners is not None:
         lowest_val = len(im)
         for (x, y) in court_corners:
             lowest_val = int(y) if y < lowest_val else lowest_val
-        mask[:lowest_val] = 0 
-    else: #guess top third is crowd
+        mask[:lowest_val] = 0
+    else:  # guess top third is crowd
         mask[:len(mask)//3] = 0
 
-    #mask all players
+    # mask all players
     buffer = 30
     for detection in detections:
         for (x1, y1, x2, y2) in detection:
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             mask[y1-buffer:y2+buffer, x1-buffer:x2+buffer] = 0
 
-    #mask bottom part of screen where score is
-    #remove static regions
+    # mask bottom part of screen where score is
+    # remove static regions
     mask[-len(mask)//4:] = 0
 
-    #remove left and right bounds where detection gets bad
+    # remove left and right bounds where detection gets bad
     mask[:, -len(mask[0])//10:] = 0
     mask[:, :len(mask[0])//10] = 0
 
     return mask
-    
-def get_affine_transform(frame, cur_frame, old_frame, old_features, court_corners, detections):
-    '''Computes the affine transform that best maps between the features in the 
-    previous frame and the current frame.  Feature correspondec is determined through
-    optical flow
-    
+
+
+def get_affine_transform(frame, cur_frame, old_frame, old_features,
+                         court_corners, detections):
+    '''Computes the affine transform that best maps between the features in the
+    previous frame and the current frame.  Feature correspondce is determined
+    through optical flow
+
     frame => current frame normal (used for displaying feature corresondences)
     cur_frame => current frame in greyscale
     old_frame => last frame in greyscale
-    old_features => previous set of features from last frame (find correspondences with)
+    old_features => previous set of features from last frame
+                    (find correspondences with)
     detections => list of object detections in xyxy
-    
-    Returns 
+
+    Returns
     M => affine transform matrix
     features => features found in the cur_frame'''
     if court_corners is None or court_corners[0] is None:
         return None, None
     mask = create_mask(cur_frame, court_corners, detections)
-    #cv2.imshow('masked image', mask*cur_frame)
-    features = cv2.goodFeaturesToTrack(cur_frame, maxCorners=200, qualityLevel=0.1, minDistance=10, mask=mask, blockSize=13)
+    features = cv2.goodFeaturesToTrack(cur_frame, maxCorners=200,
+                                       qualityLevel=0.1, minDistance=10,
+                                       mask=mask, blockSize=13)
     M = None
     if old_frame is not None and old_features is not None:
-        new_feats, _, _ = cv2.calcOpticalFlowPyrLK(old_frame, cur_frame, old_features, features, winSize=(21,21), maxLevel=5, criteria=(cv2.TERM_CRITERIA_COUNT + cv2.TERM_CRITERIA_COUNT,50,0.1))
+        criteria = (cv2.TERM_CRITERIA_COUNT + cv2.TERM_CRITERIA_COUNT, 50, 0.1)
+        new_feats, _, _ = cv2.calcOpticalFlowPyrLK(old_frame,
+                                                   cur_frame,
+                                                   old_features,
+                                                   features,
+                                                   winSize=(21, 21),
+                                                   maxLevel=5,
+                                                   criteria=criteria)
         M, _ = cv2.estimateAffinePartial2D(old_features, new_feats)
 
-        #draw line between previous and current location in diplay image
+        # draw line between previous and current location in diplay image
         for old, new in zip(old_features, new_feats):
             old_p = old.ravel().astype(np.int64)
             new_p = new.ravel().astype(np.int64)
-            cv2.line(frame, old_p, new_p, [255,0,0], 3)
+            cv2.line(frame, old_p, new_p, [255, 0, 0], 3)
 
-        
     return M, features
 
+
 def draw_projection(frame, court, H):
-    '''Project the frame onto the court image
-    
+    '''Project the frame onto the court image and show the result
+
     frame => current frame
     court => top down court view to project to
-    H => homography of the projection'''
+    H => homography of the projection
+
+    Return projected image'''
     img_proj = cv2.warpPerspective(frame, H, (court.shape[1], court.shape[0]))
     proj_on_court = np.where(img_proj == 0, court, img_proj)
     cv2.imshow('projection.png', proj_on_court)
@@ -110,62 +146,77 @@ def draw_projection(frame, court, H):
     return proj_on_court
 
 
-def update_homography(frame, court, old_frame, old_features, court_corners, detections):
-    '''Updates the homography between frame and court.  First tries to find corner by intersection
-    of Hough Lines.  If this is not possible, then updates previous court boundary with motion found
-    by the optical flow transform
-    
+def update_homography(frame, court, old_frame, old_features, court_corners,
+                      detections):
+    '''Updates the homography between frame and court.  First tries to find
+    corner by intersection of Hough Lines.  If this is not possible, then
+    updates previous court boundary with motion found by the optical flow
+    transform
+
     frame => current frame
     court => image of court to map to
     old_frame => previous frame
     old_features => features found in previous fram
-    court_corners => 4 corners marking the boundary of the court in the previous frame
+    court_corners => 4 corners marking the boundary of the court in the
+                     previous frame
     detections => list of object detections in xyxy
-    
+
     Returns
     H => homography from frame to court
-    court_corners => 4 corners marking the boundary of the court in the current frame
+    court_corners => 4 corners marking the boundary of the court in the
+                     current frame
     frame_grey => current_frame in greyscale
     features => features found in this frame'''
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #homography by lines
+    # homography by lines
     H_lines, n1, n2, n3, n4, canny = homography.get_best_frame_homography(frame, court)
-    #TODO testing affine transformation
-    #H_lines = None
-    #court_corners = np.array([n1, n2, n3, n4])
+    n_corners = np.array([n1, n2, n3, 4])
 
-    #homography by affine
-    M, features = get_affine_transform(frame, frame_gray, old_frame, old_features, court_corners, detections)
-    #TODO testing homography
-    #M = None if H_lines is not None else M
+    # homography by affine
+    M, features = get_affine_transform(frame, frame_gray, old_frame,
+                                       old_features, court_corners, detections)
+
+    # if there is an affine
     if M is not None:
+        # assign correct court orientation
         if court_side(court_corners):
-            dst_points = np.array([base_values['top-left'], base_values['top-right'], base_values['bot-left'], base_values['bot-right']])
+            dst_points = np.array([base_values['top-left'],
+                                   base_values['top-right'],
+                                   base_values['bot-left'],
+                                   base_values['bot-right']])
         else:
-            dst_points = np.array([base_values['bot-left'], base_values['bot-right'], base_values['top-left'], base_values['top-right']])
+            dst_points = np.array([base_values['bot-left'],
+                                   base_values['bot-right'],
+                                   base_values['top-left'],
+                                   base_values['top-right']])
 
         court_corners = update_drift_full(M, court_corners)
         H_affine, _ = cv2.findHomography(court_corners, dst_points)
-        
+
+        # if there is an affine and a lines pick the one with lowest line error
         if H_lines is not None:
-                
-            affine_score = (homography.eval_line(court_corners[0], court_corners[1], canny) + homography.eval_line(court_corners[0], court_corners[2], canny)) / 2
-            lines_score = (homography.eval_line(n1, n2, canny) + homography.eval_line(n1, n3, canny)) / 2
-            
-            H = H_affine if affine_score > lines_score else H_lines
-            court_corners = court_corners if affine_score > lines_score else np.array([n1, n2, n3, n4])
+
+            affine_l1 = homography.eval_line(court_corners[0], court_corners[1], canny)
+            affine_l2 = homography.eval_line(court_corners[0], court_corners[2], canny)
+            affine_score = (affine_l1 + affine_l2) / 2
+
+            lines_l1 = homography.eval_line(n1, n2, canny)
+            lines_l2 = homography.eval_line(n1, n3, canny)
+            lines_score = (lines_l1 + lines_l2) / 2
+
             if affine_score > lines_score:
-                print('AFFINE')
+                H = H_affine
+                court_corners = court_corners
             else:
-                print('Lines')
+                H = H_lines
+                court_corners = n_corners
+
         else:
             H = H_affine
-            print('AFFINE')
 
     else:
         H = H_lines
-        print('Lines')
         court_corners = np.array([n1, n2, n3, n4])
 
     # verify the projection visually
@@ -174,8 +225,11 @@ def update_homography(frame, court, old_frame, old_features, court_corners, dete
 
     return H, court_corners, frame_gray, features
 
+
 def court_side(corners):
-    '''Returns True if the court is looking at the right side'''
+    '''Returns True if the court is looking at the right side
+
+    corners => 4 court corners'''
     if corners[0][0] < corners[2][0]:
         return False
     return True
